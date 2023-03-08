d5a6b4ef8872a89ea2f747d76835d09deb58d83b
KyahWill <knownbutnotloved@gmail.com>
2023-03-08 15:10:53 +0800

    added optional_1 and optional_2

--- optional_1/test.py
+++ optional_1/test.py
@@ -2,10 +2,10 @@ import requests
 
 url = 'https://www.youtube.com'
 apiEndpoint = "http://localhost:5000/"
-
+code = 'alias'
 def get_link(url):
     response = requests.post(apiEndpoint,
-        json={'url': url},
+        json={'url': url, 'code':code},
         headers={'Content-type': 'application/json'}
     )
     return response.content.decode()
@@ -16,6 +16,6 @@ def redirect(link):
 # Print the response
 link = get_link(url)
 print(link)
-output = redirect(link) 
+# output = redirect(link) 
 
-print(output)
+# print(output)
---
+++ optional_2/app.py
@@ -0,0 +1,34 @@
+import serverless_wsgi
+from flask import Flask,request, redirect,Response
+
+app = Flask(__name__)
+alias_url_dictionary = {}
+
+@app.route('/',methods = ['POST'])
+def createShortenedLink():
+    req= request.get_json()
+
+    if 'url' not in req.keys():
+        return{'message':'Error! Missing URL'}, 404
+    url = req['url']
+    
+    if 'code' not in req.keys():
+        return {'message':'Error! Missing Code'}, 404
+    alias = req['code']
+
+    if alias in alias_url_dictionary.keys():
+        return {'message':'Error! Alias is already made'}, 400
+    
+    alias_url_dictionary[alias] = url
+    message = request.base_url + alias
+    return {"message":message}, 200
+
+@app.route('/<string:code>')
+def redirectShortenedLink(code):
+    if code in alias_url_dictionary.keys():
+        return redirect(alias_url_dictionary[code], code=302)
+    else:
+        return 500
+    
+def handler(event,context):
+    return serverless_wsgi.handle_request(app,event,context)
